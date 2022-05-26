297eb5bc27fb834f5de6bc44b41301d758b551fd
Anouki Panthagani <anouki@csil.cs.ucsb.edu>
2022-05-26 15:09:03 -0700

    ap - added coursesadded entity and repository

---
+++ src/main/java/edu/ucsb/cs156/courses/entities/CoursesAdded.java
@@ -0,0 +1,37 @@
+package edu.ucsb.cs156.courses.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "coursesadded")
+public class CoursesAdded {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+  //private String id;
+  private String enrollCd;
+  private String psId;
+  private String quarter;
+}
+
+// id
+// enrollCd (enrollment code)
+// psId (personal schedule id)
+// yyyyq (the quarter in yyyyq format)
