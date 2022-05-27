687bdac9896a65bdd419e6a3aaff7b7c328135fa
Hyun Kyum Kim <hyunkyumkim@ucsb.edu>
2022-05-26 18:52:17 -0700

    hkk - corrected Detail menu bar name & added personal schedule detail page

---
+++ frontend/src/main/pages/PersonalSchedules/PersonalSchedulesDetailPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function PersonalSchedulesDetailPage() {
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                <h1>PersonalSchedules Detail</h1>
+                <p>
+                    This is where the detail page will go
+                </p>
+            </div>
+        </BasicLayout>
+    )
+}
