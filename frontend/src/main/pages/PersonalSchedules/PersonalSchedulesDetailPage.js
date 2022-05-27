f79cd6792ffc204cc7ae1f5ee087cb4eab595105
Hyun Kyum Kim <hyunkyumkim@ucsb.edu>
2022-05-26 19:03:41 -0700

    hkk - added temporary api endpoint for personal schedule detail page

--- frontend/src/main/pages/PersonalSchedules/PersonalSchedulesDetailPage.js
+++ frontend/src/main/pages/PersonalSchedules/PersonalSchedulesDetailPage.js
@@ -1,6 +1,25 @@
+import React from 'react'
+//import { useBackend } from 'main/utils/useBackend'
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useCurrentUser } from 'main/utils/currentUser';
+import { useBackend } from 'main/utils/useBackend';
+//import PersonalSchedulesDetailTable from 'main/components/PersonalSchedulesDetailTable';
+// Remember to ask -1 team for components for DetailTable and edit based on that
+//import { useCurrentUser } from 'main/utils/currentUser'
 
 export default function PersonalSchedulesDetailPage() {
+
+    // const currentUser = useCurrentUser();
+
+    // const { data: personalSchedulesDetail, error: _error, status: _status } =
+    //     useBackend(
+    //         // Stryker disable next-line all : don't test internal caching of React Query
+    //         ["/api/coursesadded/all"], // Edit based on -2 team api endpoint
+    //         { method: "GET", url: "/api/courseadded/all" },
+    //         []
+    //     );
+
     return (
         <BasicLayout>
             <div className="pt-2">
