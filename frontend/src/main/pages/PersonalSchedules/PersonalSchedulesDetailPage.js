479be0f54969bc80fa2ef708a8b38635a2b26d8e
Hyun Kyum Kim <hyunkyumkim@ucsb.edu>
2022-05-30 16:03:08 -0700

    hkk - removed commented out part

--- frontend/src/main/pages/PersonalSchedules/PersonalSchedulesDetailPage.js
+++ frontend/src/main/pages/PersonalSchedules/PersonalSchedulesDetailPage.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
-//import { useBackend } from 'main/utils/useBackend'
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import { _useCurrentUser } from 'main/utils/currentUser';
-//import { useBackend } from 'main/utils/useBackend';
-//import PersonalSchedulesDetailTable from 'main/components/PersonalSchedulesDetailTable'
-// Remember to ask -1 team for components for DetailTable and edit based on that
 
 export default function PersonalSchedulesDetailPage() {
 
     let { id } = useParams();
-
-    const currentUser = useCurrentUser();
-
-    // const { data: personalSchedulesDetail, error: _error, status: _status } =
-    //     useBackend(
-    //         // Stryker disable next-line all : don't test internal caching of React Query
-    //         ["/api/coursesadded/all"], // Edit based on -2 team api endpoint
-    //         { method: "GET", url: "/api/courseadded/all" },
-    //         []
-    //     );
 
     return (
         <BasicLayout>
