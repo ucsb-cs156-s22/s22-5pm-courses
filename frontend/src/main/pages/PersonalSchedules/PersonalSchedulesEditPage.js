d1dfc1981cf34c26fe6a8a8285d5a1c3d3f3438e
Jesus Oviedo <jaoviedo@ucsb.edu>
2022-05-30 23:24:50 -0700

    jo - Merged 'react-router-dom import statements

--- frontend/src/main/pages/PersonalSchedules/PersonalSchedulesEditPage.js
+++ frontend/src/main/pages/PersonalSchedules/PersonalSchedulesEditPage.js
@@ -1,7 +1,6 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import PersonalScheduleForm from "main/components/PersonalSchedules/PersonalScheduleForm";
-import { Navigate } from 'react-router-dom'
 import { useBackend, useBackendMutation } from "main/utils/useBackend";
 import { toast } from "react-toastify";
 
