7f38e3338072b0e85d39c3b3716e2e9fe6f10ef2
Alexander Ward <alexanderward@ucsb.edu>
2022-06-02 16:17:54 -0700

    AW - Resolved some merge issues

--- src/main/java/edu/ucsb/cs156/courses/documents/CoursePage.java
+++ src/main/java/edu/ucsb/cs156/courses/documents/CoursePage.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
