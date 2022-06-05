3506ee1b6a32d9259aea2389b0a6bdcb557dc0f8
jacksonjude <jacksonjude@mac.com>
2022-06-04 20:27:54 -0700

    Created AddedCoursesController + tests

---
+++ src/main/java/edu/ucsb/cs156/courses/controllers/AddedCoursesController.java
@@ -0,0 +1,106 @@
+package edu.ucsb.cs156.courses.controllers;
+
+import edu.ucsb.cs156.courses.entities.PersonalSchedule;
+import edu.ucsb.cs156.courses.entities.AddedCourse;
+import edu.ucsb.cs156.courses.entities.User;
+import edu.ucsb.cs156.courses.errors.EntityNotFoundException;
+import edu.ucsb.cs156.courses.models.CurrentUser;
+import edu.ucsb.cs156.courses.repositories.PersonalScheduleRepository;
+
+import edu.ucsb.cs156.courses.repositories.AddedCourseRepository;
+
+import edu.ucsb.cs156.courses.entities.AddedCourse;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+import edu.ucsb.cs156.courses.services.UCSBCurriculumService;
+
+@Api(description = "AddedCourses")
+@RequestMapping("/api/addedcourses")
+@RestController
+@Slf4j
+public class AddedCoursesController extends ApiController {
+
+    @Autowired
+    PersonalScheduleRepository personalscheduleRepository;
+
+    @Autowired
+    AddedCourseRepository addedCourseRepository;
+
+    @Autowired
+    UCSBCurriculumService ucsbCurriculumService;
+
+    @ApiOperation(value = "Get sections from a single schedule")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin")
+    public ResponseEntity<List<String>> thisScheduleSections(
+            @ApiParam("id") @RequestParam Long id
+    ) {
+        System.out.println("Hello, testing admin");
+        PersonalSchedule personalSchedule = personalscheduleRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException(PersonalSchedule.class, id));
+
+        var quarter = personalSchedule.getQuarter();
+        Iterable<AddedCourse> classesAdded = addedCourseRepository.findAllByPersonalSchedule(personalSchedule);
+        List<String> listOfJSON = new ArrayList<>();
+
+        for(AddedCourse currentClass : classesAdded)
+        {
+            String enrollCode = currentClass.getEnrollCd();
+            String currentSection = ucsbCurriculumService.getSectionJSON(quarter, enrollCode);
+            listOfJSON.add(currentSection);
+        }
+
+        return ResponseEntity.ok().body(listOfJSON);
+    }
+
+
+    @ApiOperation(value = "Get sections from a single schedule (if it belongs to current user)")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public ResponseEntity<List<String>> thisUserSections(
+            @ApiParam("id") @RequestParam Long id) {
+        User currentUser = getCurrentUser().getUser();
+        PersonalSchedule personalSchedule = personalscheduleRepository.findByIdAndUser(id, currentUser)
+          .orElseThrow(() -> new EntityNotFoundException(PersonalSchedule.class, id));
+
+        var quarter = personalSchedule.getQuarter();
+        Iterable<AddedCourse> classesAdded = addedCourseRepository.findAllByPersonalSchedule(personalSchedule);
+        List<String> listOfJSON = new ArrayList<>();
+        for(AddedCourse currentClass : classesAdded)
+        {
+            String enrollCode = currentClass.getEnrollCd();
+            String currentSection = ucsbCurriculumService.getSectionJSON(quarter, enrollCode);
+            listOfJSON.add(currentSection);
+        }
+
+        return ResponseEntity.ok().body(listOfJSON);
+    }
+
+}
---
+++ src/test/java/edu/ucsb/cs156/courses/controllers/AddedCoursesControllerTests.java
@@ -0,0 +1,172 @@
+package edu.ucsb.cs156.courses.controllers;
+
+import edu.ucsb.cs156.courses.repositories.UserRepository;
+import edu.ucsb.cs156.courses.services.CurrentUserService;
+import edu.ucsb.cs156.courses.services.UCSBCurriculumService;
+import edu.ucsb.cs156.courses.testconfig.TestConfig;
+import edu.ucsb.cs156.courses.ControllerTestCase;
+import edu.ucsb.cs156.courses.entities.AddedCourse;
+import edu.ucsb.cs156.courses.entities.PersonalSchedule;
+import edu.ucsb.cs156.courses.entities.User;
+import edu.ucsb.cs156.courses.repositories.PersonalScheduleRepository;
+import edu.ucsb.cs156.courses.repositories.AddedCourseRepository;
+
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import com.mongodb.DuplicateKeyException;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.ucsb.cs156.courses.models.CurrentUser;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@WebMvcTest(controllers = AddedCoursesController.class)
+@Import(TestConfig.class)
+public class AddedCoursesControllerTests extends ControllerTestCase {
+
+    @MockBean
+    PersonalScheduleRepository personalscheduleRepository;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @MockBean
+    AddedCourseRepository addedCourseRepository;
+
+    @MockBean
+    UCSBCurriculumService ucsbcirService;
+
+    @MockBean
+    CurrentUserService currentUserService;
+
+    @Autowired
+    public ObjectMapper mapper;
+
+    @Test
+    public void sections_admin_all__logged_out__returns_403() throws Exception {
+        mockMvc.perform(get("/api/addedcourses/admin?id=1"))
+                .andExpect(status().is(403));
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void sections_admin_all__user_logged_in__returns_403() throws Exception {
+        mockMvc.perform(get("/api/addedcourses/admin?id=1"))
+                .andExpect(status().is(403));
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void sections__user_logged_in__returns_404() throws Exception {
+        mockMvc.perform(get("/api/addedcourses2?id=1"))
+                .andExpect(status().is(404));
+    }
+
+
+    @WithMockUser(roles = { "ADMIN" })
+    @Test
+    public void sections_admin_logged_in__returns_404() throws Exception {
+        mockMvc.perform(get("/api/addedcourses/admin?id=1"))
+                .andExpect(status().is(404));
+    }
+
+
+
+    @WithMockUser(roles = { "ADMIN" })
+    @Test
+    public void sections_admin_all_id_does_not_exist() throws Exception {
+
+        when(addedCourseRepository.findById(eq(7L))).thenReturn(Optional.empty());
+
+        MvcResult response = mockMvc.perform(get("/api/addedcourses/admin?id=7"))
+                                .andExpect(status().is(404)).andReturn();
+
+        verify(personalscheduleRepository, times(1)).findById(eq(7L));
+        Map<String, Object> json = responseToJson(response);
+        assertEquals("EntityNotFoundException", json.get("type"));
+        assertEquals("PersonalSchedule with id 7 not found", json.get("message"));
+    }
+
+    @WithMockUser(roles = { "ADMIN" })
+    @Test
+    public void sections_admin_return_OK() throws Exception {
+
+        User u1 = User.builder().id(1L).build();
+
+        PersonalSchedule personalSchedule = PersonalSchedule.builder().name("Ryan").description("Test").quarter("2022W").user(u1).id(1L).build();
+        when(personalscheduleRepository.findById(1L)).thenReturn(Optional.of(personalSchedule));
+
+        AddedCourse ac1 = AddedCourse.builder().enrollCd("123").personalSchedule(personalSchedule).id(1).build();
+        List<AddedCourse> listac1 = new ArrayList<AddedCourse>();
+        listac1.add(ac1);
+        when(addedCourseRepository.findAllByPersonalSchedule(personalSchedule)).thenReturn(listac1);
+        when(ucsbcirService.getSectionJSON("2022W","123")).thenReturn("Section Test");
+
+        MvcResult response = mockMvc.perform(get("/api/addedcourses/admin?id=1"))
+                                .andExpect(status().isOk()).andReturn();
+
+        verify(personalscheduleRepository, times(1)).findById(eq(1L));
+        String responseString = response.getResponse().getContentAsString();
+        List<String> resultList =  mapper.readValue(responseString, List.class);
+        System.out.println("JSON" + resultList);
+        assertEquals("Section Test", resultList.get(0));
+    }
+
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void sections_user_return_OK() throws Exception {
+        User u1 = User.builder().id(1L).build();
+        CurrentUser curUser = CurrentUser.builder().user(u1).build();
+        when(currentUserService.getCurrentUser()).thenReturn(curUser);
+
+
+        PersonalSchedule personalSchedule = PersonalSchedule.builder().name("Ryan").description("Test").quarter("2022W").user(u1).id(1L).build();
+        when(personalscheduleRepository.findByIdAndUser(1L, u1)).thenReturn(Optional.of(personalSchedule));
+
+        AddedCourse ac1 = AddedCourse.builder().enrollCd("123").personalSchedule(personalSchedule).id(1).build();
+        List<AddedCourse> listac1 = new ArrayList<AddedCourse>();
+        listac1.add(ac1);
+        when(addedCourseRepository.findAllByPersonalSchedule(personalSchedule)).thenReturn(listac1);
+
+        when(ucsbcirService.getSectionJSON("2022W","123")).thenReturn("Section Test");
+
+        MvcResult response = mockMvc.perform(get("/api/addedcourses?id=1"))
+                                .andExpect(status().isOk()).andReturn();
+
+        verify(personalscheduleRepository, times(1)).findByIdAndUser(1L, u1);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String responseString = response.getResponse().getContentAsString();
+        List<String> resultList =  mapper.readValue(responseString, List.class);
+        System.out.println("JSON" + resultList);
+        assertEquals("Section Test", resultList.get(0));
+
+    }
+
+}
