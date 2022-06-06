d5e4fc417b15adcede76eb6b2859e0778febe0b0
Alexander Ward <77415714+alexward120@users.noreply.github.com>
2022-06-05 19:48:00 -0700

    Removed Unneeded White Space

--- frontend/src/tests/components/Sections/SectionsTable.test.js
+++ frontend/src/tests/components/Sections/SectionsTable.test.js
@@ -71,8 +71,7 @@ describe("UserTable tests", () => {
             </QueryClientProvider>
         );
 
-        const expectedHeaders = ["Quarter", "Course Number", "Course Title", "Days", "Begin Time", "End Time", 
-        "Enrolled", "Max. Enrollment"];
+        const expectedHeaders = ["Quarter", "Course Number", "Course Title", "Days", "Begin Time", "End Time", "Enrolled", "Max. Enrollment"];
         const expectedFields = ["quarter", "courseInfo.courseId", "courseInfo.title", "section.timeLocations[0].days", 
         "section.timeLocations[0].beginTime", "section.timeLocations[0].endTime", "section.enrolledTotal", "section.maxEnroll"];
         const testId = "SectionsTable";
@@ -105,8 +104,7 @@ describe("UserTable tests", () => {
             </QueryClientProvider>
         );
 
-        const expectedHeaders = ["Quarter", "Course Number", "Course Title", "Days", "Begin Time", "End Time", 
-        "Enrolled", "Max. Enrollment"];
+        const expectedHeaders = ["Quarter", "Course Number", "Course Title", "Days", "Begin Time", "End Time", "Enrolled", "Max. Enrollment"];
         const expectedFields = ["quarter", "courseInfo.courseId", "courseInfo.title", "section.timeLocations[0].days",
         "section.timeLocations[0].beginTime", "section.timeLocations[0].endTime", "section.enrolledTotal", "section.maxEnroll"];
         const testId = "SectionsTable";
